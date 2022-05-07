91273a1bd008b2e6435604d90f177c1881d681d2
ariloc <sefide77@gmail.com>
2022-05-07 18:16:40 -0300

    Tidied up some code. In process of adding new sections to the main page.

--- scripts.js
+++ scripts.js
@@ -114,7 +114,32 @@ $(document).ready(function(){
                                           "padding-right": ""});
             next();
         });
+    });
 
+
+    $(".carousel").owlCarousel({
+        margin: 30,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+
+            0:{
+                items:1,
+                nav: false
+            },
+
+            600:{
+                items:2,
+                nav: false
+            },
+
+            1000:{
+                items:3,
+                nav: false
+            }
+        }
     });
 });
 
