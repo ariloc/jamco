97d95afca4f660d067bf6058ae56cd5c078a457d
ariloc <sefide77@gmail.com>
2022-05-18 01:33:55 -0300

    Carousel fixed, and IS responsive
    Also fixed some minor details

--- scripts.js
+++ scripts.js
@@ -97,6 +97,17 @@ $(document).ready(function(){
         $("#search-close-mobile").css({"display": "flex"}).hide(0).fadeIn(200);
     });
 
+    /* even though we have css for this, the way we reuse the desktop search bar
+       may lead into a situation where the search bar isn't visible anymore after
+       resizing the viewport (i.e. the window of your browser in a desktop) */
+    $(window).on('resize', function() {
+        if ($(this).width() > 600) {
+            $("div.search-bar").show(0);
+            $("#search-wrapper").css("width","");
+        }
+        else $("div.search-bar").hide(0);
+    });
+
     $("#search-close-mobile").click(function() {
         $(this).queue(function(next){
             searchBoxFocusOut();
@@ -117,158 +128,172 @@ $(document).ready(function(){
     });
 
 
-    // -- NEW CAROUSEL -- (WIP)
-
-    // TODO: Establece el ancho (hacer responsive)
-    // TODO: Hacer que se actualice al resizear (PD: podría aplicarse también al navbar)
-
+    // -- NEW CAROUSEL --
     var carousel_items = 5;
+    $(".carousel-card").clone().appendTo($('.main-carousel-cards')); // duplicate the elements so it seems infinite
+    
     var carousel = $('.main-carousel-cards'),
+    carousel_card = $('.main-carousel-cards .carousel-card'),
+    carousel_links = $('.main-carousel a'),
     threshold = 150,
     slideWidth = 300,
+    slideDuration = 6000,
     dragStart, 
     dragEnd;
-    var carousel_card = $('.main-carousel-cards .carousel-card');
 
-    function refreshCarouselViewport() {
+    function refreshCarouselViewport() { // from 1 to 5 items depending on the width of the viewport
         carousel_items = 1 + ($(this).width() > 600) +
-                                  ($(this).width() > 800) +
-                                  ($(this).width() > 1000) +
-                                  ($(this).width() > 1200);
+                             ($(this).width() > 800) +
+                             ($(this).width() > 1000) +
+                             ($(this).width() > 1200);
 
         var carousel_card_width = (carousel.width() / carousel_items);
         carousel_card.css("min-width", carousel_card_width + "px");
         slideWidth = carousel_card_width;
-        carousel.css("left", "-" + ((carousel[0].scrollWidth / 2) - (carousel.width() / 2)) + "px");
+        carousel.css("left", "-" + ((carousel[0].scrollWidth / 2) - (carousel.width() / 2) + slideWidth / 2) + "px");
     }
 
     $(window).on('resize', function() {
         refreshCarouselViewport(); // when the viewport is resized
     });
-    refreshCarouselViewport(); // when the page loads
 
-    function dragMainCarousel() {
-        var carousel = $(".main-carousel-cards");
-        var current_drag = parseFloat(carousel.css("transform").split(',')[4]);
-        carousel.animate({
-            duration: 6000,
-            step: function(now, fn) {
-                //fn.start = current_drag;
-                //fn.end = current_drag + slideWidth;
-                //carousel.css("transform", "translateX(" + now + "px)");
-                console.log("EEEH");
-            },
-            done: function(){console.log("EHY");}
-        })
+    function getTranslateX (e) {
+        return parseFloat(e.css("transform").split(',')[4]);
     }
 
-    var interval = setInterval(dragMainCarousel,6000);
+    function dragMainCarousel() {
+        var current_drag = getTranslateX(carousel);
+        var duration = Math.abs((slideDuration / slideWidth) * ((-slideWidth) - current_drag));
+
+        carousel.animate({now: "--"}, {
+            duration: duration,
+            easing: 'linear',
+            step: function(now, fn) {
+                fn.start = current_drag;
+                fn.end = -slideWidth;
+                $(this).css("transform", "translateX(" + now + "px)");
+            },
+            done: function(){
+                $('.main-carousel .carousel-card:last').after($('.main-carousel .carousel-card:first'));
+                $(this).css("transform", "translateX(0px)");
+                dragMainCarousel();
+            }
+        });
+    }
+
+    refreshCarouselViewport(); // when the page loads
     dragMainCarousel();
 
-    $('#next').click(function(){ shiftSlide(-1) })
-    $('#prev').click(function(){ shiftSlide(1) })
+    var carousel_hover_timeout, carousel_blur_timeout, restart_carousel_timeout;
+    var hover_carousel = false, dragging_carousel = false, carousel_stopped = false;
+    function restartCarouselAnimation() {
+        clearTimeout(restart_carousel_timeout);
+        restart_carousel_timeout = setTimeout(function() {
+            if (!hover_carousel && !dragging_carousel && carousel_stopped){
+                carousel_stopped = false;
+                dragMainCarousel();
+            }
+        },1000);
+    }
 
-    
-    carousel.on('mousedown', function(){
+    var carousel_all = $(".main-carousel *");
+    carousel_all.on('mouseenter touchstart', function() {
+        hover_carousel = true;
+        clearTimeout(carousel_hover_timeout);
+        clearTimeout(carousel_blur_timeout);
+        carousel_hover_timeout = setTimeout(function(){                
+            carousel.stop(true);
+            carousel_stopped = true;
+            restartCarouselAnimation();
+        },300);
+    });
+
+    carousel_all.on('mouseleave touchend touchcancel', function() {
+        hover_carousel = false;
+        clearTimeout(carousel_hover_timeout);
+        clearTimeout(carousel_blur_timeout);
+        restartCarouselAnimation();
+    });
+
+    function getTouchX(e) {
+        var evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent;
+        var touch = evt.touches[0] || evt.changedTouches[0];
+        return touch.pageX;
+    }
+
+    carousel.on('mousedown touchstart', function(e){
+        if (e.type == 'mousedown') e.preventDefault();
+        $(this).css("cursor","grabbing");
+        dragging_carousel = true;
+
         if (carousel.hasClass('transition')) return;
-        dragStart = event.pageX;
-        $(this).on('mousemove', function(){
-            dragEnd = event.pageX;
-            $(this).css('transform','translateX('+ dragPos() +'px)')
+        carousel.stop(true);
+        carousel_stopped = true;
+        dragStart = (e.type == 'mousedown' ? event.pageX : getTouchX(e)) - getTranslateX(carousel);
+
+        var movement_detected = false;
+        $(this).on('mousemove touchmove', function(e){
+            dragEnd = (e.type == 'mousemove' ? event.pageX : getTouchX(e));
+            $(this).css('transform','translateX('+ dragPos() +'px)');
+            carousel_links.css("cursor","grabbing");
+            movement_detected = true;
         })
-        $(document).on('mouseup', function(){
-            if (dragPos() > threshold) { return shiftSlide(1) }
-            if (dragPos() < -threshold) { return shiftSlide(-1) }
-            shiftSlide(0);
-        })
+
+        /* Using namespace "main-carousel" for mouseup event in $(document),
+           because removing the event handler with .off() disabled another
+           listener that was used for the navbar. */
+        $(document).on('mouseup.main-carousel touchend.main-carousel', function() {
+            carousel.css("cursor", "");
+            carousel_links.css("cursor","");
+            $(document).off('.main-carousel');
+            carousel.off('mousemove touchmove');
+            dragging_carousel = false;
+            carousel.mouseleave();
+
+            if (!movement_detected) return;
+
+            setTimeout(function(){movement_detected = false;}, 200);
+
+            var cards_passed = parseInt(dragPos() / slideWidth);
+            var offset_to_last = dragPos() - cards_passed * slideWidth;
+            if (offset_to_last > threshold) { cards_passed++; }
+            if (offset_to_last < -threshold) { cards_passed--; }
+            shiftSlide(cards_passed);
+        });
+
+        
+        carousel_links.on('click', function(e) {
+            if(movement_detected) {
+                e.preventDefault();
+            }
+            else {
+                /* avoid carousel getting stuck after clicking in a link
+                   and going back */
+                carousel.mouseleave();
+            }
+        });
     });
 
     function dragPos() {
         return dragEnd - dragStart;
     }
 
-
-
     function shiftSlide(direction) {
         if (carousel.hasClass('transition')) return;
         dragEnd = dragStart;
-        $(document).off('mouseup')
-        carousel.off('mousemove')
-                .addClass('transition')
+        carousel.addClass('transition')
                 .css('transform','translateX(' + (direction * slideWidth) + 'px)'); 
         setTimeout(function(){
-            if (direction === 1) {
+            while (direction > 0) {
                 $('.main-carousel .carousel-card:first').before($('.main-carousel .carousel-card:last'));
-            } else if (direction === -1) {
+                direction--;
+            }
+            while(direction < 0) {
                 $('.main-carousel .carousel-card:last').after($('.main-carousel .carousel-card:first'));
+                direction++;
             }
-        carousel.removeClass('transition')
+            carousel.removeClass('transition')
             carousel.css('transform','translateX(0px)'); 
-        },700)
+        },200);
     }
-
-    // -- CAROUSEL --
-    $(".carousel").owlCarousel({
-        margin: 50,
-        loop: true,
-        autoplay: true,
-        autoplayTimeout: 6000,
-        smartSpeed: 6000,
-        slideTransition: 'linear',
-        autoplayHoverPause: false,
-        mouseDrag: false,
-        center: true,
-        responsive: {
-
-            0:{
-                items:1,
-                nav: false
-            },
-
-            600:{
-                items:2,
-                nav: false
-            },
-
-            800:{
-                items:3,
-                nav: false
-            },
-
-            1000:{
-                items:4,
-                nav: false
-            },
-
-            1200:{
-                items:5,
-                nav: false
-            }
-        }
-    });
-
-    $(".carousel").trigger('next.owl'); // Avoids the first 6s waiting time
-    
-    // -- END CAROUSEL --
 });
-
-/*const carousel = document.querySelector(".carousel owl-carousel");
-
-let intervalo = null;
-
-const start = () => {
-    intervalo = setInterval(function(){
-        carousel.scrollLeft = carousel.scrollLeft + 5;
-    },10)
-};
-
-const stop = () => {
-
-};
-
-start();*/
-
-
-
-
-
