ddad8f3f12f5da81dffe9e3fa514e3a95fa86700
Gerartam <102983369+Gerartam@users.noreply.github.com>
2022-06-03 01:01:20 -0300

    Add files via upload

---
+++ public/js/login.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+    $(".link").click(function(){
+        $(".login").hide();
+        $(".register").show(200);
+    });
+
+    
+});
--- public/login.php
+++ public/login.php
@@ -6,10 +6,11 @@
 
         <div class="main-wrapper">
             <?php include "navbar.html"; ?>
-            
-            <form class="login">
-                <h1 style="margin-top: 40px;margin-bottom: 0px;">Iniciar sesión</h1>
-                <div class="cosa">
+
+            <div class="form_login_register">
+                <form class="login">
+                    <h1 style="margin-top: 40px; margin-bottom: 0px;">Iniciar sesión</h1>
+                    <div class="cosa">
         
                         <div class="input-container">
                             <input type="username" placeholder="Nombre de usuario">
@@ -18,15 +19,43 @@
                         <div class="input-container">
                             <input type="password" placeholder="Contraseña">
                         </div>
-            
+
                         <input type="submit" value="Iniciar sesión" class="button" style="margin-top: 20px;margin-bottom: 20px;">
+
                         <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
                         <p>
                             ¿No tienes una cuenta?
-                            <a class="link" href="register.html">Registrate!</a>
+                            <a class="link">Registrate!</a>
                         </p>
-                </div>
-            </form>
+                    </div>
+                </form>
+                 
+                <form class="register">
+                    <h1 style="margin-top: 40px; margin-bottom: 0px;">Registro</h1>
+                    <div class="cosa">
+        
+                        <div class="input-container">
+                            <input type="username" placeholder="Nombre de usuario">
+                        </div>
+
+                        <div class="input-container">
+                            <input type="username" placeholder="Correo electrónico">
+                        </div>
+                        
+                        <div class="input-container">
+                            <input type="password" placeholder="Contraseña">
+                        </div>
+
+                        <div class="input-container">
+                            <input type="password" placeholder="Confirmar contraseña">
+                        </div>
+            
+                        <input type="submit" value="Registrarse" class="button" style="margin-top: 20px;margin-bottom: 20px;">
+                        <p>Al registrarte, aceptas nuestras Condiciones de uso y Política de privacidad.</p>
+                    </div>
+                </form>
+            </div>
+            
         </div>
     </body>
 </html>
