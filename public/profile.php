098563edb0a16dd7269f5f307ac3566a95ac08f5
Mateo Pretelli <86113904+mateopretelli@users.noreply.github.com>
2022-06-11 21:53:44 -0300

    Add files via upload

---
+++ public/profile.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+    <?php include "header.php"; ?>  
+
+    <body>
+        <div class="profile-top">
+            <div class="profile-frontpage" style="background-image: url('img/backgrounds/fondo.jpg');">
+                <div class="profile-pic">
+                    <img src="img/artists/rick-astley-2.png" class="img-aux">
+                    <!--<a href="#" class="change-pfp">
+                    <i class="fa fa-camera"></i>
+                    <span>Cambiar Foto</span>
+                    </a>-->
+                </div>
+
+                <div class="profile-data">
+                    <h4 class="username">Nombre de usuario</h4> 
+                    <ul class="profile-stats">
+                        <li>X Seguidores</li>
+                        <li>Y Seguidos</li>
+                        <li>Z Reseñas</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </body>
+
+</html> 
