7f0e5bff3b71676f6f79ed79d78df89c999324da
ariloc <sefide77@gmail.com>
2022-06-16 09:48:54 -0300

    Deleted unnecessary columns. Changed nickname charset to utf8mb4.

--- tables.sql
+++ tables.sql
@@ -1,2 +1,17 @@
-CREATE TABLE users(id INT NOT NULL AUTO_INCREMENT, username VARCHAR(32) NOT NULL UNIQUE, email VARCHAR(64) NOT NULL UNIQUE, password VARCHAR(255) NOT NULL, valid_state TINYINT NOT NULL DEFAULT 0, activation_token VARCHAR(255), country_code CHAR(2), profile_pic VARCHAR(255), front_page_pic VARCHAR(255), created_at TIMESTAMP DEFAULT current_timestamp(), login_times INT NOT NULL DEFAULT 0, last_login TIMESTAMP NULL, nickname VARCHAR(128) CHARACTER SET utf8, birth DATETIME, activation_expiry TIMESTAMP NULL, PRIMARY KEY (id));
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT, 
+    username VARCHAR(32) NOT NULL UNIQUE, 
+    email VARCHAR(64) NOT NULL UNIQUE, 
+    password VARCHAR(255) NOT NULL, 
+    valid_state TINYINT NOT NULL DEFAULT 0,
+    activation_token VARCHAR(255),
+    country_code CHAR(2), 
+    created_at TIMESTAMP DEFAULT current_timestamp(), 
+    login_times INT NOT NULL DEFAULT 0, 
+    last_login TIMESTAMP NULL, 
+    nickname VARCHAR(128) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci, 
+    birth DATETIME, 
+    activation_expiry TIMESTAMP NULL,
+    PRIMARY KEY (id)
+);
 
