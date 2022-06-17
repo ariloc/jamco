b40b977ee4327d1df45b94e61878c434cc24ccb2
Gerartam <102983369+Gerartam@users.noreply.github.com>
2022-06-17 16:34:58 -0300

    Add files via upload

--- public/profile.php
+++ public/profile.php
@@ -40,6 +40,7 @@
                         <a href="/chainsmoker">
                             <img src="img/communities/chainsmoker.png">
                         </a>
+                        <h6>Chainsmokers</h6>
                     </div>
                 </div>
                 <div>
@@ -47,6 +48,7 @@
                         <a href="/chainsmoker">
                             <img src="img/communities/chainsmoker.png">
                         </a>
+                        <h6>Chainsmokers</h6>
                     </div>
                 </div>
                 <div>
@@ -54,6 +56,7 @@
                         <a href="/chainsmoker">
                             <img src="img/communities/chainsmoker.png">
                         </a>
+                        <h6>Me dueles</h6>
                     </div>
                 </div>
                 <div>
@@ -61,6 +64,7 @@
                         <a href="/chainsmoker">
                             <img src="img/communities/chainsmoker.png">
                         </a>
+                        <h6>La vecinita</h6>
                     </div>
                 </div>
                 <div>
@@ -68,40 +72,61 @@
                         <a href="/chainsmoker">
                             <img src="img/communities/chainsmoker.png">
                         </a>
+                        <h6>Never Gonna Give You Up</h6>
                     </div>
                 </div>
             </div>
         </div>
 
-        <div class="profile-review">
-            <div class="profile-review-left">
-                <div class="profile-review-header">
-                    <div class="profile-review-header-data">
-                        <a href="/review">Ultimas reseña</a>
-                        <a href="/profile">Nombre_de_Usuario</a>
-                    </div>
-                </div>
-                <div class="profile-review-text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum aspernatur doloremque animi impedit consequuntur est nisi error ipsum, mollitia doloribus 
-                    autem repellat, tenetur itaque, ut quo ea blanditiis dignissimos distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur hic, facere
-                    exercitationem dolores eveniet distinctio! Tempora, commodi. Laudantium enim cum, corporis quas eius consequuntur illum voluptatibus aspernatur ut nulla voluptatum!</p>
-                </div>
-            </div>
+        <div class="wrap">
+		    <ul class="tabs">
+		    	<li><a href="#tab1"><span class="fa fa-bookmark"></span><span class="tab-text">Reseñas</span></a></li>
+		    	<li><a href="#tab2"><span class="fa fa-group"></span><span class="tab-text">Comunidades</span></a></li>
+		    	<li><a href="#tab3"><span class="fa fa-briefcase"></span><span class="tab-text">Canciones</span></a></li>
+		    </ul>
+
+		    <div class="secciones">
+		    	<article id="tab1">
+                    <div class="profile-review">
+                        <div class="profile-review-left">
+                            <div class="profile-review-header">
+                                <div class="profile-review-header-data">
+                                    <a href="/review">Ultimas reseña</a>
+                                    <a href="/profile">Nombre_de_Usuario</a>
+                                </div>
+                            </div>
+                            <div class="profile-review-text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum aspernatur doloremque animi impedit consequuntur est nisi error ipsum, mollitia doloribus 
+                                autem repellat, tenetur itaque, ut quo ea blanditiis dignissimos distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur hic, facere
+                                exercitationem dolores eveniet distinctio! Tempora, commodi. Laudantium enim cum, corporis quas eius consequuntur illum voluptatibus aspernatur ut nulla voluptatum!</p>
+                            </div>
+                        </div>
             
-            <div class="profile-review-right">
-                <div class="profile-review-header">
-                    <div class="profile-review-header-data">
-                        <a href="/review">Ultimas reseña</a>
-                        <a href="/profile">Nombre_de_Usuario</a>
+                        <div class="profile-review-right">
+                            <div class="profile-review-header">
+                                <div class="profile-review-header-data">
+                                    <a href="/review">Ultimas reseña</a>
+                                    <a href="/profile">Nombre_de_Usuario</a>
+                                </div>
+                            </div>
+                            <div class="profile-review-text">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum aspernatur doloremque animi impedit consequuntur est nisi error ipsum, mollitia doloribus 
+                                autem repellat, tenetur itaque, ut quo ea blanditiis dignissimos distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur hic, facere
+                                exercitationem dolores eveniet distinctio! Tempora, commodi. Laudantium enim cum, corporis quas eius consequuntur illum voluptatibus aspernatur ut nulla voluptatum!</p>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div class="profile-review-text">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum aspernatur doloremque animi impedit consequuntur est nisi error ipsum, mollitia doloribus 
-                    autem repellat, tenetur itaque, ut quo ea blanditiis dignissimos distinctio. Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur hic, facere
-                    exercitationem dolores eveniet distinctio! Tempora, commodi. Laudantium enim cum, corporis quas eius consequuntur illum voluptatibus aspernatur ut nulla voluptatum!</p>
-                </div>
-            </div>
+		    	</article>
+		    	<article id="tab2">
+		    		<h1>Comunidades</h1>
+		    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel voluptates unde, consequuntur aliquid architecto rem numquam expedita minima dolorem pariatur recusandae, eius quod quia aspernatur id impedit, tenetur! Aspernatur incidunt molestiae dolores animi ea praesentium ipsam tenetur voluptas cupiditate perspiciatis eum nihil, natus exercitationem libero earum fuga dignissimos impedit numquam, quasi, placeat officiis voluptates, ad reprehenderit fugiat? Fugiat aperiam et magni, molestiae, numquam consectetur vitae sapiente cupiditate totam laboriosam voluptate obcaecati, aliquam placeat? Suscipit dolores fuga laudantium sed, qui magni iusto dolore quia. Quis fugit exercitationem porro. Rerum nihil omnis recusandae ratione fuga alias eligendi, earum sunt veritatis praesentium eum perspiciatis. Molestias deserunt, iure neque animi quod! Impedit reprehenderit cumque, numquam velit quae cum eius quidem similique laudantium hic deleniti!</p>
+		    	</article>
+		    	<article id="tab3">
+		    		<h1>Canciones</h1>
+		    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea numquam odio voluptate. Aliquam incidunt similique, et quasi ducimus quos aut autem non dignissimos dicta sit provident, voluptatibus ut blanditiis perspiciatis cum, vel temporibus minima enim. Asperiores omnis placeat officiis a tenetur sit recusandae, reprehenderit neque. Tempora quibusdam, perferendis id ratione culpa dolorum! Nemo, animi?</p><br>
+		    		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum dignissimos at esse, ipsum rerum assumenda nisi obcaecati! Aliquam iure voluptatem incidunt, explicabo sit labore, perferendis eius ad vel quia. Praesentium, doloribus. Quisquam provident nostrum totam itaque debitis, minima, tempore dolores!</p>
+		    	</article>
+		    </div>
         </div>
-
     </body>
 </html>
