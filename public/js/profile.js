f3e1c2f95849276983e691024cad8e1d2147302c
Gerartam <102983369+Gerartam@users.noreply.github.com>
2022-06-17 16:35:44 -0300

    Add files via upload

--- public/js/profile.js
+++ public/js/profile.js
@@ -10,10 +10,26 @@ $(document).ready(function() {
             {
                 breakpoint: 1024,
                 settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 3
+                    slidesToShow: 3,
+                    slidesToScroll: 2
                 }
             }
-        ]
+        ],
+    });
+});
+
+$(document).ready(function() {
+    $('ul.tabs li a:first').addClass('active');
+    $('.secciones article').hide();
+	$('.secciones article:first').show();
+
+    $('ul.tabs li a').click(function(){
+    	$('ul.tabs li a').removeClass('active');
+    	$(this).addClass('active');
+    	$('.secciones article').hide();
+
+    	var activeTab = $(this).attr('href');
+    	$(activeTab).show();
+    	return false;
     });
 });
