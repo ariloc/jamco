efc27c37b1c7ca3a397041d8df8db85b191d4574
Ariel Leonardo Fideleff <sefide77@gmail.com>
2022-08-22 00:25:40 +0000

    Merged animations for tabs

--- public/js/profile.js
+++ public/js/profile.js
@@ -41,33 +41,42 @@ $(document).ready(function() {
             }
           ]
     });
-});
 
-$(document).ready(function() {
-    $('ul.tabs li a:first').addClass('active');
-    $('.secciones article').hide();
-	$('.secciones article:first').show();
+    initial_left = $('.tab-indicator').css("left");
+    secciones = $('.secciones');
+    active_tab = secciones.children('.active');
+    inactive_tabs = secciones.children('.inactive');
 
-    $('ul.tabs li a').click(function(){
-    	$('ul.tabs li a').removeClass('active');
-    	$(this).addClass('active');
-    	$('.secciones article').hide();
+    $('.tabs > div').each(function(i) {
+        $(this).click(function() {
+            $('.tab-indicator').css("left", `calc(calc(calc(100% / 3) * ${i}) + ${initial_left})`);
 
-    	var activeTab = $(this).attr('href');
-    	$(activeTab).show();
-    	return false;
+            active_tab.empty();
+            var selected = inactive_tabs.children().eq(i);
+            var copy = selected.clone().appendTo(active_tab);
+
+            copy.animate({opacity: 1}, {duration: 300, queue: false});
+            $({y:-10}).animate({y:0}, {
+                duration: 300,
+                step: function(val) {
+                    copy.css("transform", `translateY(${val}px)`);
+                },
+                queue: false
+            });
+
+            secciones.animate({height: active_tab[0].scrollHeight}, {duration: 300, queue: false});
+            $('html, body').animate({
+                scrollTop: $(".tabs-container").offset().top - 30
+            }, {duration: 400, queue: false});
+        });
     });
+    
+    $(window).resize(function(){
+        secciones.css("height", active_tab[0].scrollHeight);
+    });
+
+    // show first tab on load
+    inactive_tabs.children().eq(0).clone().appendTo(active_tab);
+    secciones.css({"height": active_tab[0].scrollHeight});
+    active_tab.children().eq(0).css({"opacity": 1, "transform": "translateY(0px)"});
 });
-
-/*const parentContainer = document.querySelector('profile-review-text');
-
-parentContainer.addEventListener('click',event>={
-  
-  const current = event.target;
-
-  const isReadMoreBtn = current.className.includes('read-more-btn');
-
-  if(isReadMoreBtn) return;
-
-  const currenttext = event.target.parentNode.querySelector('.read-more-text');
-})*/
